35c25cc8f767da6668b33b96293e794cfb5d48fc
João Paulo <joao_paulinh@hotmail.com>
2022-06-13 23:16:34 -0300

    Add page Manga

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,41 +2,41 @@ import { ArrowRight, ArrowLeft } from 'phosphor-react'
 import { useMangas } from '../contexts/MangasContext'
 
 export function Pagination() {
-  const { page, loadMangas } = useMangas()
+  const { pageSettings, loadMangas } = useMangas()
 
   return (
     <div className="flex items-center justify-center gap-[15px] py-[20px] w-[100%] text-[1.3rem] font-bold text-pink-600">
-      {page.current_page > 1 && (
+      {pageSettings.current_page > 1 && (
         <ArrowLeft
           className="text-[1.5rem] cursor-pointer"
-          onClick={() => loadMangas(page.current_page - 1)}
+          onClick={() => loadMangas(pageSettings.current_page - 1)}
         />
       )}
-      {page.current_page > 2 && (
+      {pageSettings.current_page > 2 && (
         <span className="cursor-pointer" onClick={() => loadMangas(1)}>
           1
         </span>
       )}
-      {page.current_page > 1 && (
+      {pageSettings.current_page > 1 && (
         <span
           className="cursor-pointer"
-          onClick={() => loadMangas(page.current_page - 1)}
+          onClick={() => loadMangas(pageSettings.current_page - 1)}
         >
-          ... {page.current_page - 1}
+          ... {pageSettings.current_page - 1}
         </span>
       )}
       <span className="text-pink-400 font-extrabold cursor-pointer">
-        {page.current_page}
+        {pageSettings.current_page}
       </span>
       <span
         className="cursor-pointer"
-        onClick={() => loadMangas(page.current_page + 1)}
+        onClick={() => loadMangas(pageSettings.current_page + 1)}
       >
-        {page.current_page + 1} ...
+        {pageSettings.current_page + 1} ...
       </span>
       <ArrowRight
         className="text-[1.5rem] cursor-pointer"
-        onClick={() => loadMangas(page.current_page + 1)}
+        onClick={() => loadMangas(pageSettings.current_page + 1)}
       />
     </div>
   )
