491765a4c50cd7ed92d1c7387e784ea3d39a6737
João Paulo <joao_paulinh@hotmail.com>
2022-06-21 18:20:23 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,29 +2,10 @@ import { Header } from '../components/Header'
 import backgroundHome from '../assets/background.jpg'
 import { ListMangas } from '../components/ListMangas'
 import { Footer } from '../components/Footer'
-import { useMangas } from '../contexts/MangasContext'
-import { useEffect } from 'react'
-import { Pagination } from '../components/Pagination'
 import { ListTopCharacters } from '../components/ListTopCharacters'
 import { ListTopMangas } from '../components/ListTopMangas'
 
 export default function Home() {
-  const {
-    favoriteCharacter,
-    isLoadingCharacter,
-    pageCharacters,
-    loadFavoriteCharacter,
-    isLoadingBestMangas,
-    loadTopMangas,
-    pageTopMangas,
-    topMangas
-  } = useMangas()
-
-  useEffect(() => {
-    loadFavoriteCharacter(1)
-    loadTopMangas(1)
-  }, [])
-
   return (
     <div>
       <img src={backgroundHome} className="fixed top-0 left-0 z-[-1]" />
